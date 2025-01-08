05d8c38d93b471ca85e2f856eb4ecd87945b08e3
ClumsyPenguin <seppe318@gmail.com>
2025-01-08 14:57:18 +0100

    Added basic HTTP Trigger for Azure Function and setup for Azurite

---
+++ server/open-weather/OpenWeather.Azure.Function.LoadOpenMeteoData/Controllers/TemperatureController.cs
@@ -0,0 +1,23 @@
+using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace OpenWeather.Azure.Function.LoadOpenMeteoData.Controllers;
+
+public class TemperatureController(ILogger<TemperatureController> logger)
+{
+    private readonly ILogger<TemperatureController> _logger = logger;
+
+    [Function(nameof(GetCurrentTemperature))]
+    [OpenApiOperation(operationId: "Run")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(int), Description = "The OK response message containing the current temperature")]
+    public IActionResult GetCurrentTemperature(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequest req,
+        ILogger log)
+    {
+        return new OkObjectResult(1);
+    }
+}
