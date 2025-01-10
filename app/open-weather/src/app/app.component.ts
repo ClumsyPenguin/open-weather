f2c0b2b6062f48cbccfd3974a3788dd17f07fdff
ClumsyPenguin <seppe318@gmail.com>
2025-01-10 19:34:35 +0100

    Added footer and accomodated the template for it

--- app/open-weather/src/app/app.component.ts
+++ app/open-weather/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, RouterLink, NavbarComponent],
+  imports: [RouterModule, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
