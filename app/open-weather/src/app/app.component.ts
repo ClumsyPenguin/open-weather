460e31a5894e1dab9aeba86925da3a2f7c54f6a2
ClumsyPenguin <seppe318@gmail.com>
2025-01-10 14:35:40 +0100

    Added skeleton html for general ui

--- app/open-weather/src/app/app.component.ts
+++ app/open-weather/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from "./components/menu/menu.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
