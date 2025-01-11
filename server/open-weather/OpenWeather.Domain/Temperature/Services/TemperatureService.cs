ef5f1e210d6b91ada5e3dca1796f93dee6c42492
ClumsyPenguin <seppe318@gmail.com>
2025-01-11 11:46:55 +0100

    Diverted from default cache expiration time for temperature data

--- server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
+++ server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
@@ -9,9 +9,16 @@ public class TemperatureService(HybridCache hybridCache) : ITemperatureService
     public async Task<double> GetCurrentTemperatureAsync(GetCurrentTemperatureRequest request,
         CancellationToken cancellationToken)
     {
+        var entryOptions = new HybridCacheEntryOptions
+        {
+            Expiration = TimeSpan.FromHours(1), //KMI updates weather data at intervals of 1 hour
+            LocalCacheExpiration = TimeSpan.FromHours(1),
+        };
+        
         return await hybridCache.GetOrCreateAsync(
             $"temperature-{request.Longitude}-{request.Latitude}",
             async _ => await Task.FromResult(21d),
+            entryOptions,
             tags: ["temperature"],
             cancellationToken: cancellationToken
         );
