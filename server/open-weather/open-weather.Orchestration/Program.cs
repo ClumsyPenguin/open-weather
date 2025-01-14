a49355734acf6af6f45c1bd722de77e8054964bd
ClumsyPenguin <seppe318@gmail.com>
2025-01-14 16:31:08 +0100

    Added Redis without the Aspire dependency in my client

--- server/open-weather/open-weather.Orchestration/Program.cs
+++ server/open-weather/open-weather.Orchestration/Program.cs
@@ -1,5 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.OpenWeather_Adapters_REST>("openweather-adapters-rest");
+var redisCache = builder
+    .AddRedis("cache", port: 6379)
+    .WithImage("redis/redis-stack");
+    
+
+builder
+    .AddProject<Projects.OpenWeather_Adapters_REST>("openweather-adapters-rest")
+    .WithReference(redisCache);
 
 builder.Build().Run();
