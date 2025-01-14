f1209b822ca4b8dc417de093723bdcde79b518ea
ClumsyPenguin <seppe318@gmail.com>
2025-01-14 17:21:04 +0100

    Added PostgreSQL to Aspire

--- server/open-weather/open-weather.Orchestration/Program.cs
+++ server/open-weather/open-weather.Orchestration/Program.cs
@@ -2,11 +2,17 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var redisCache = builder
     .AddRedis("cache", port: 6379)
-    .WithImage("redis/redis-stack");
-    
+    .WithImage("redis/redis-stack")
+    .WithImageTag("latest");
+
+var postGreSqlDb = builder
+    .AddPostgres("db")
+    .WithPgAdmin()
+    .WithImageTag("latest");
 
 builder
     .AddProject<Projects.OpenWeather_Adapters_REST>("openweather-adapters-rest")
-    .WithReference(redisCache);
+    .WithReference(redisCache)
+    .WithReference(postGreSqlDb);
 
 builder.Build().Run();
