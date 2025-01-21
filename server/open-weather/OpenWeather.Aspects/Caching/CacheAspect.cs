fd7b8e62b3d13eaa270c00afe80503d96020d7be
Bart Salmon <bartje_salmon@hotmail.com>
2025-01-21 21:35:55 +0100

    * First attemp at making cachign an aspect

---
+++ server/open-weather/OpenWeather.Aspects/Caching/CacheAspect.cs
@@ -0,0 +1,51 @@
+﻿using Metalama.Extensions.DependencyInjection;
+using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+using Microsoft.Extensions.Caching.Hybrid;
+using System.Threading;
+
+namespace OpenWeather.Aspects.Caching
+{
+    public class CacheAttribute : OverrideMethodAspect
+    {
+        [IntroduceDependency]
+        private readonly HybridCache _hybridCache;
+        private readonly int _durationInHours;
+
+        public CacheAttribute(int durationInHours)
+        {
+            _durationInHours = durationInHours;
+        }
+
+        public override dynamic? OverrideMethod()
+        {
+          
+            var entryOptions = new HybridCacheEntryOptions
+            {
+                Expiration = TimeSpan.FromHours(_durationInHours), 
+                LocalCacheExpiration = TimeSpan.FromHours(_durationInHours),
+            };
+
+            CancellationToken cancellationToken = meta.Target.Method.Parameters["cancellationToken"].Value;
+
+            return _hybridCache.GetOrCreateAsync<CacheAttribute, object>(CreateKey(meta.Target.Method),
+                                                (state, cancel) => state.ExecuteMethod(cancellationToken),
+                                                entryOptions,
+                                                cancellationToken: cancellationToken);
+            
+        }
+
+        [RunTime]
+        private async ValueTask<object> ExecuteMethod(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CreateKey(IMethod method)
+        {
+            var parameterValues = string.Join('-',method.Parameters.Select(p => p));
+
+            return $"{method.Name}-{parameterValues}";
+        }
+    }
+}
--- server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
+++ server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
@@ -1,26 +1,13 @@
-using Microsoft.Extensions.Caching.Hybrid;
 using OpenWeather.Domain.Temperature.Models;
 using OpenWeather.Domain.Temperature.Services.Ports;
 
 namespace OpenWeather.Domain.Temperature.Services;
 
-internal class TemperatureService(HybridCache hybridCache) : ITemperatureService
-{
-    public async Task<double> GetCurrentTemperatureAsync(GetCurrentTemperatureRequest request,
-        CancellationToken cancellationToken)
+internal class TemperatureService() : ITemperatureService
+{//KMI updates weather data at intervals of 1 hour
+    public Task<double> GetCurrentTemperatureAsync(GetCurrentTemperatureRequest request, CancellationToken cancellationToken)
     {
-        var entryOptions = new HybridCacheEntryOptions
-        {
-            Expiration = TimeSpan.FromHours(1), //KMI updates weather data at intervals of 1 hour
-            LocalCacheExpiration = TimeSpan.FromHours(1),
-        };
-        
-        return await hybridCache.GetOrCreateAsync(
-            $"temperature-{request.Longitude}-{request.Latitude}",
-            async _ => await Task.FromResult(21d),
-            entryOptions,
-            tags: ["temperature"],
-            cancellationToken: cancellationToken
-        );
+
+        return Task.FromResult(21d);
     }
 }
