921b93c68ea17944bbc8ba3c52359169ca866193
Bart Salmon <bartje_salmon@hotmail.com>
2025-01-23 14:57:12 +0100

    Set-up cache for temperature service

--- server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
+++ server/open-weather/OpenWeather.Domain/Temperature/Services/TemperatureService.cs
@@ -1,3 +1,4 @@
+using OpenWeather.Aspects.Caching;
 using OpenWeather.Domain.Temperature.Models;
 using OpenWeather.Domain.Temperature.Services.Ports;
 
@@ -5,6 +6,7 @@ namespace OpenWeather.Domain.Temperature.Services;
 
 internal class TemperatureService() : ITemperatureService
 {//KMI updates weather data at intervals of 1 hour
+    [Cache(60)]
     public Task<double> GetCurrentTemperatureAsync(GetCurrentTemperatureRequest request, CancellationToken cancellationToken)
     {
 
